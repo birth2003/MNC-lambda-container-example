004ee7fc85ec53025633370069ba3fc0d5d91fb4
birth2003 <88282228+birth2003@users.noreply.github.com>
2024-07-19 16:37:05 +0900

    lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,8 +8,9 @@ def lambda_handler(event, context):
     model = MobileNet(weights='imagenet')
     img_array = np.load("/var/task/MNC-lambda-container-example/preprocessed_image.npy")
     preds = model.predict(img_array)
+    prediction = decode_predictions(preds, top=1)[0]
 
     return {
-        'Predicted:', decode_predictions(preds, top=1)[0]
+        'Predicted:', prediction
     }
     
